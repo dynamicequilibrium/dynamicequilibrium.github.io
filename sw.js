d7b39f13fa4da4db9a7ccc2b87d6b24a1d10d0c3
Alexander Lebedev MD/PhD <4508892+alex-lebedev@users.noreply.github.com>
2025-10-14 21:22:22 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Basic Service Worker for PWA functionality
+const CACHE_NAME = 'dynamic-equilibrium-v1';
+const urlsToCache = [
+  '/',
+  '/ru/',
+  '/images/logo-black.svg',
+  '/images/logo-white.svg',
+  '/images/Alexander.webp',
+  '/images/Alexander.jpg',
+  '/site.webmanifest'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
